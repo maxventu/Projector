6efd62ea6221b1ecfce75e843c60611c9361b5b9
maxventu <1995_maxim@mail.ru>
2016-04-05 12:47:07 +0300

    delete everything

--- src/main/java/com/itechart/kalenik/dao/repository/SubtaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itechart.kalenik.dao.repository;
-
-import com.itechart.kalenik.dao.entity.Subtask;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubtaskRepository extends JpaRepository<Subtask, Integer> {
-}
